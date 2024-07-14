727d9cd73f8e8fb098cd22e6548051318eecaa5e
d3Cod3Monk <50435516+d3cod3monk78@users.noreply.github.com>
2024-07-15 00:22:50 +0530

    Update c1-15.py

--- chapter-01/c1-15.py
+++ chapter-01/c1-15.py
@@ -1,3 +1,4 @@
+from typing import Set
 import random
 
 
@@ -12,8 +13,23 @@ def checking_distinct_list(data: [int]) -> bool:
     return True
 
 
+def approaching_with_set(data: [int]) -> bool:
+    new_set: Set[int] = set()
+
+    for x in data:
+        if x in new_set:
+            return False
+        else:
+            new_set.add(x)
+    return True
+
+
 data_list: [int] = [random.randrange(1, 21) for _ in range(11)]
 another_data_list: [int] = [x for x in range(1,11)]
 print(data_list)
 print(checking_distinct_list(data_list))
 print(checking_distinct_list(another_data_list))
+
+print()
+print(approaching_with_set(data_list))
+print(approaching_with_set(another_data_list))
